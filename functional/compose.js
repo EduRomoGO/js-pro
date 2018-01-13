1951ec761006b0f39aaaa827addde5c1b20f5292
EduRomoGO <eduromogo@outlook.es>
2018-01-13 16:33:20 +0100

    fix compose and add more examples

--- functional/compose.js
+++ functional/compose.js
@@ -1,33 +1,17 @@
-const {pack} = require('./functionalLib.js');
-
-function compose (a, b) {
-    return (...args) => {
-        return a(b(...args));
-    };
-}
-
-function compose(...fns) {
-    const [head, ...tail] = fns.reverse();
-    return (...args) => {
-        return reduce(
-            (acc, fn) => fn(acc),
-            head(...args),
-            tail
-        );
-    };
-}
+const {pack, compose} = require('./functionalLib.js');
 
 const mult = (a, b) => a * b;
 const half = (a) => a/2;
 
 const triangleArea = compose(half, mult);
-// const triangleAreaPacked = compose(half, pack(mult));
-// console.log(triangleAreaPacked);
-const triangle = {
-    base: 2,
+const triangleAreaPacked = compose(half, pack(mult));
+const myTriangle = {
+    base: 4,
     height: 5
-}
+};
+const getAreaData = triangle => [triangle.base, triangle.height]
+const areaFromTriangle = compose(triangleAreaPacked, getAreaData);
 
-console.log(triangleArea(2, 5));
-console.log(Object.values(triangle));
-// console.log(triangleArea(triangleAreaPacked(Object.values(triangle))));
+console.log(triangleArea(4, 5));
+console.log(Object.values(myTriangle));
+console.log(areaFromTriangle(myTriangle));
