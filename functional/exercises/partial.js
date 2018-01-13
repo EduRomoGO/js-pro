73ec2306963f8207557213bb736bf41d8fb68bf5
EduRomoGO <eduromogo@outlook.es>
2018-01-13 15:46:45 +0100

    add partial exercises

--- functional/exercises/partial.js
+++ functional/exercises/partial.js
@@ -1,38 +1,28 @@
 // Given a collection of writters (in data.js), 
 // write a function to correct a typo in their incomes 
-// that returns a new collection where each writter incomes are multiplied by 1000
+// that returns a new array whith only the incomes corrected multiplied by 1000
 // Note: Name the function correctIncomes
 // Log the result in the console
 
 const {writters} = require('./data.js');
 const {partial} = require('../functionalLib.js');
 
-// wrong, doesnt return a new collection
+// wrong, doesnt return a new array
 // function correctIncomes () {
 //     writters.forEach(item => item.incomes * 1000);
 // }
 
 // correct
-function correctIncomes () {
-    return writters.map(item => {
-        const itemCopy = Object.assign({}, item);
-
-        itemCopy.incomes *= 1000;
-
-        return itemCopy;
-    });
-}
+// function correctIncomes () {
+//     return writters.map(item => item.incomes * 1000);
+// }
 
 
 // using partial
 const mult = (a, b) => a * b;
 const mult1000 = partial(mult, 1000);
 function correctIncomes () {
-    const itemCopy = Object.assign({}, item);
-    
-    itemCopy.incomes *= 1000;
-    
-    return itemCopy;
+    return writters.map(item => item.incomes).map(mult1000);
 }
 
 
