8d979f3aff841edc6d6684dfd50fd62c0d65240a
EduRomoGO <eduromogo@outlook.es>
2018-01-14 13:59:05 +0100

    rename files and add tests for other exercises

--- functional/exercises/basics.js
+++ functional/exercises/basics.js
@@ -1,19 +1,12 @@
-// Given a collection of writters (in data.js), 
-// write a function to correct a typo in their incomes 
-// that returns a new array whith only the incomes corrected multiplied by 1000
-// Note: Name the function correctIncomes
-// Log the result in the console
-
-const {writters} = require('./data.js');
 const {partial, compose} = require('../functionalLib.js');
 
 // wrong, doesnt return a new array
-// function correctIncomes () {
+// function correctIncomes (writters) {
 //     writters.forEach(item => item.incomes * 1000);
 // }
 
 // correct
-// function correctIncomes () {
+// function correctIncomes (writters) {
 //     return writters.map(item => item.incomes * 1000);
 // }
 
@@ -21,7 +14,7 @@ const {partial, compose} = require('../functionalLib.js');
 // using partial
 // const mult = (a, b) => a * b;
 // const mult1000 = partial(mult, 1000);
-// function correctIncomes () {
+// function correctIncomes (writters) {
 //     return writters.map(item => item.incomes).map(mult1000);
 // }
 
@@ -30,10 +23,9 @@ const mult = (a, b) => a * b;
 const mult1000 = partial(mult, 1000);
 const getIncomes = writer => writer.incomes;
 
-function correctIncomes () {
+function correctIncomes (writters) {
     return writters.map(compose(mult1000, getIncomes));
 }
 
-const correctWritters = correctIncomes();
-console.log(writters);
-console.log(correctWritters);
+
+module.exports = {correctIncomes};
--- functional/test/execisesTest.js
+++ functional/test/execisesTest.js
@@ -1,5 +1,7 @@
 const {pack, range, currify, compose, map, partial, partialRight, first} = require('../functionalLib.js');
 const {fizzBuzz, functionalFizzBuzz} = require('../exercises/fizzBuzz.js');
+const {writters} = require('./data.js');
+const {correctIncomes} = require('../exercises/basics.js');
 
 const chai = require('chai');
 const expect = chai.expect;
@@ -16,6 +18,13 @@ describe('exercises', () => {
 
         it('should return the same for both modes', () => {
             expect(fizzBuzz()).to.deep.equal(functionalFizzBuzz());
+        });
+    });
+
+
+    describe('Given a collection of writters (in data.js), correctIncomes function', () => {
+        it ('should correct a typo in their incomes that returns a new array whith only the incomes corrected multiplied by 1000', () => {
+            expect(correctIncomes(writters)).to.deep.equal([ 93000, 44000, 98000, 13000 ]);
         });
     });
 });
